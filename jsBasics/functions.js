a29dc65962c35d336122a1ff379a6dff7068b8a1
Vitasta Kachroo <vitastakachroo@gmail.com>
2023-05-27 00:02:18 +0530

    Introduction to functions in js

---
+++ jsBasics/functions.js
@@ -0,0 +1,27 @@
+// PART 2
+
+
+// Declaring a function
+function reusableFunction(){
+    console.log("Hello World!");
+}
+
+reusableFunction()
+reusableFunction()
+
+console.log("\n")
+
+// Passing values to functions with arguements
+function funcWithArgs(a,b){
+    sum = a + b;
+    console.log(sum)
+}
+
+funcWithArgs(9,3)
+funcWithArgs("Hello ","Universe")
+
+console.log("\n")
+
+// Global Scope and Functions
+
+
